c9920100fda5fdd69bd085d88e84815c1ae6b2ee
Roman Micuda <roman.micuda@outlook.com>
2024-06-22 09:53:22 +0200

    chore: create react application for notes

---
+++ README.md
@@ -0,0 +1,3 @@
+# Web application for notes
+
+This is web application for notes in React
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        Notes
+    </div>
+  );
+}
+
+export default App;
